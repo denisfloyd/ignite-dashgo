6a4a59ec2b988b99d9b45dbc55745af1c1bccffa
denismend <denisladeira1@gmail.com>
2021-10-16 15:53:03 -0300

    feat:

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,7 +18,6 @@ import {
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from "react-query";
-import { useEffect } from "toasted-notes/node_modules/@types/react";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -29,7 +28,20 @@ export default function UserList() {
     const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
 
-    return data;
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }),
+      };
+    });
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -85,34 +97,26 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px="6">
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Denis Ladeira</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          denisladeira1@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      >
-                        {isWideVersion ? "Editar" : ""}
-                      </Button>
-                    </Td>
-                  </Tr>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px="6">
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
-
               <Pagination />
             </>
           )}
@@ -120,4 +124,18 @@ export default function UserList() {
       </Flex>
     </Box>
   );
+}
+
+{
+  /* <Td>
+  <Button
+    as="a"
+    size="sm"
+    fontSize="sm"
+    colorScheme="purple"
+    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+  >
+    {isWideVersion ? "Editar" : ""}
+  </Button>
+</Td> */
 }
