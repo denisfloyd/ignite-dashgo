79dd27399dfa09781de552e2e3d8d156ac5ed19b
denismend <denisladeira1@gmail.com>
2021-11-15 11:27:00 -0300

    refactor: :art: react query mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,13 +9,17 @@ import {
   Button,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useForm, SubmitHandler } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "react-query";
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   name: string;
@@ -25,13 +29,38 @@ type CreateUserFormData = {
 };
 
 const createUserFormSchema = yup.object().shape({
-  name: yup.string().required('Nome Obrigatória'),
+  name: yup.string().required("Nome Obrigatória"),
   email: yup.string().email("E-mail inválido").required("E-mail obrigatório"),
-  password: yup.string().required("Senha obrigatória").min(6, "Senha deve ter no mínimo 6 caracteres"),
-  password_confirmation: yup.string().oneOf([yup.ref("password"), null], "Senhas não conferem")
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "Senha deve ter no mínimo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Senhas não conferem"),
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
+
   const {
     register,
     handleSubmit,
@@ -40,11 +69,14 @@ export default function CreateUser() {
     resolver: yupResolver(createUserFormSchema),
   });
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(values);
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
+
+    router.push("/users");
   };
-  
+
   return (
     <Box>
       <Header />
@@ -52,7 +84,14 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box as="form" flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} onSubmit={handleSubmit(handleCreateUser)}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">
             Criar Usuário
           </Heading>
@@ -61,12 +100,29 @@ export default function CreateUser() {
 
           <VStack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome completo" error={errors.name} {...register("name")}/>
-              <Input name="email" type="email" label="E-mail" error={errors.email}{...register("email")}/>
+              <Input
+                name="name"
+                label="Nome completo"
+                error={errors.name}
+                {...register("name")}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email}
+                {...register("email")}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="password" type="password" label="Senha" error={errors.password} {...register("password")}/>
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                error={errors.password}
+                {...register("password")}
+              />
               <Input
                 name="password_confirmation"
                 type="password"
@@ -84,7 +140,9 @@ export default function CreateUser() {
                   Cancelar
                 </Button>
               </Link>
-              <Button type="submit" isLoading={isSubmitting} colorScheme="pink">Salvar</Button>
+              <Button type="submit" isLoading={isSubmitting} colorScheme="pink">
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
